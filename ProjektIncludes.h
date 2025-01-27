3c1238d5ce567be0ffef5a9d173443679c9f49f0
U-BJA\bjafrge <bjafrge@BJAPC180.bja.de>
2025-01-27 13:17:20 +0100

    erste

---
+++ ProjektIncludes.h
@@ -0,0 +1,21 @@
+ 
+
+#ifndef PROJECT_INCLUDES
+#define PROJECT_INCLUDES
+
+    #include <sys/time.h>
+    #include <sys/types.h>
+    #include <sys/socket.h>
+    #include <netinet/in.h>
+    #include <arpa/inet.h>
+    #include <stdio.h>
+    #include <fstream>
+    #include <iostream>
+    #include <ncurses.h>
+
+    using namespace std;
+
+    //void* SPRGeneratedStartThread_SHTTPDWorker(void* iThreadParam);
+
+#endif
+
